f1b116f1998586d2f86e1873867b70296c109a5d
Roldán Sanchis Martínez <rolsanmar@alu.edu.gva.es>
2025-05-02 14:54:29 +0000

    Releciones eliminadas de __init__.py, models eliminado por completo

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # Tables 
-from . import models
 from . import client
 from . import item
 from . import journey
@@ -11,8 +10,4 @@ from . import pack
 from . import route
 
 # Relationships
-from . import lodgin_by_journey
-from . import lodgin_by_pack
-from . import lodgin_by_route
-from . import item_by_pack
 from . import landmark_by_route
